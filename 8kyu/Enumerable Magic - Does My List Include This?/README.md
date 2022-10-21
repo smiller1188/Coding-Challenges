9ab5a87599d3b7ce2f4ac053d06a8820d275c1c1
Sarah Miller <97858648+smiller1188@users.noreply.github.com>
2022-10-20 20:13:34 -0700

    Add files via upload

---
+++ 8kyu/Enumerable Magic - Does My List Include This?/README.md
@@ -0,0 +1,3 @@
+DESCRIPTION:
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
---
+++ 8kyu/Enumerable Magic - Does My List Include This?/main.js
@@ -0,0 +1,8 @@
+function include(arr, item){
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] === item) {
+        return true
+      }
+    }
+        return false
+  }
