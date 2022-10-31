ce629b71664b3ee2b4e06de55059d73b0fdfa69c
Sarah Miller <97858648+smiller1188@users.noreply.github.com>
2022-10-30 20:02:33 -0700

    Add files via upload

---
+++ 8kyu/Basic Mathematical Operations/main.js
@@ -0,0 +1,12 @@
+function basicOp(operation, value1, value2)
+{
+  if(operation == '+') {
+    return value1 + value2
+  } else if(operation == '-') {
+    return value1 - value2
+  } else if(operation == '*') {
+    return value1 * value2
+  } else if(operation == '/') {
+    return value1 / value2
+  }
+}
