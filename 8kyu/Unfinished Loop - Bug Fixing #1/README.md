5a80ce29d1b1bf1b9ea669353b7a9855bef4d52e
Sarah Miller <97858648+smiller1188@users.noreply.github.com>
2023-01-22 20:24:26 -0800

    Add files via upload

---
+++ 8kyu/Unfinished Loop - Bug Fixing #1/README.md
@@ -0,0 +1,4 @@
+DESCRIPTION:
+
+Unfinished Loop - Bug Fixing #1
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
---
+++ 8kyu/Unfinished Loop - Bug Fixing #1/mian.js
@@ -0,0 +1,9 @@
+function createArray(number){
+    var newArray = [];
+    
+    for(let i = 1; i <= number; i++){
+      newArray.push(i);
+    }
+    
+    return newArray;
+  }
